bb5e5eb744066724105acd05514ed7d0be1c5c35
Jonas De Nauw <jonas.de.nauw@aclvb.be>
2026-04-30 09:12:53 +0200

    Add hello world page and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HelloWorldPageComponent } from './pages/hello-world/hello-world-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HelloWorldPageComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
---
+++ src/app/pages/hello-world/hello-world-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-world-page',
+  standalone: true,
+  templateUrl: './hello-world-page.component.html',
+  styleUrl: './hello-world-page.component.css'
+})
+export class HelloWorldPageComponent {}
